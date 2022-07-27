5f931e20b92eb3dec2b78a87476120dac48e3bd1
Sahilsom08 <107542538+Sahilsom08@users.noreply.github.com>
2022-07-27 22:32:15 +0530

    Create duplicate.cpp

---
+++ duplicate.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+//find second duplicate element in array
+//O(n)
+int fun(int arr[],int n)
+{
+    
+    int i,ans=0;
+   for( i=0;i<n;i++)
+   {
+       ans=ans^arr[i];
+   }
+   for( i=1;i<n;i++)
+   {
+       ans=ans^i;
+   }
+   return ans;
+}
+int main()
+{
+    int arr[5]={1,3,2,4,3};
+    cout<<fun(arr,5)<<endl;
+
+    int odd[7]={1,2,3,4,5,6,2};
+    cout<<fun(odd,7)<<endl;
+
+   int even[6]={1,2,4,3,4,5};
+   cout<<fun(even,6)<<endl;
+   return 0;
+}
